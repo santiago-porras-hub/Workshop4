d83d641ba2e8bc5556ed3b494a66d14994ed36e4
santiago porras <sporrass@unbosque.edu.co>
2021-04-25 14:41:28 -0500

    login

--- src/main/java/edu/unbosque/Workshop4/Login.java
+++ src/main/java/edu/unbosque/Workshop4/Login.java
@@ -1,32 +1,45 @@
 package edu.unbosque.Workshop4;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet( name="login" , value="/login")
+@WebServlet( name="loginWithCookies" , value="/login-with-cookies")
 public class Login extends HttpServlet {
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException{
+    public void doGet(HttpServletRequest request, HttpServletResponse response)throws IOException{
 
         response.setContentType("text/html");
 
         String name = request.getParameter("userName");
         String password = request.getParameter("UserPassword");
 
-        PrintWriter out= response.getWriter();
 
-        if(name.equals("unbosque") && password.equals("1234"))
 
-            out.print("Login succes..");
+        try {
+            PrintWriter out= response.getWriter();
+            if (name.equals("unbosque") && password.equals("1234")) {
 
-         else
-            out.println("Login Failed");
+                Cookie c1 = new Cookie("userName", name);
+                Cookie c2 = new Cookie("UserPassword", password);
+
+                response.addCookie(c1);
+                response.addCookie(c2);
+
+                out.print("WELCOME :" + name );
+                out.println("<br/ >");
+                out.print("<a href='welcome'> Too see Details (Cookies) please tap here </a> ");
+
+            } else
+                out.println("Login Failed");
             out.close();
-
+        }catch (Exception exp){
+            System.out.println(exp);
+        }
 
     }
 }
--- src/main/java/edu/unbosque/Workshop4/Servlet1.java
+++ src/main/java/edu/unbosque/Workshop4/Servlet1.java
@@ -1,2 +1,38 @@
-package edu.unbosque.Workshop4;public class Servlet1 {
+package edu.unbosque.Workshop4;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@WebServlet( name="welcome", value="/welcome")
+public class Servlet1 extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+
+        response.setContentType("text/html");
+
+        try {
+          Cookie[] cookies = request.getCookies();
+
+            PrintWriter out= response.getWriter();
+
+
+            for (int i=0; i<cookies.length;i++){
+
+                out.println("<br/ >");
+                out.println(cookies[i].getName()+": "+cookies[i].getValue());
+
+            }
+            out.close();
+
+        }catch (Exception exp){
+
+            System.out.println(exp);
+        }
+
+    }
 }

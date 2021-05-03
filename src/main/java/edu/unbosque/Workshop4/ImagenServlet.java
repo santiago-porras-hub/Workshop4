9f612aa949642d24f98ec344c0037e5f0dc78abb
Santiago Acevedo <sacevedor@unbosque.edu.co>
2021-05-03 18:07:17 -0500

    creating alphanumeric combination

--- src/main/java/edu/unbosque/Workshop4/ImagenServlet.java
+++ src/main/java/edu/unbosque/Workshop4/ImagenServlet.java
@@ -17,13 +17,25 @@ import java.util.Random;
 @Singleton
 public class ImagenServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
+        
         String description = request.getParameter("description");
         PrintWriter out = response.getWriter();
 
         Part filePart = request.getPart("file");
         String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+
+        char[] chars = "abcdefghijklmnopqrstuvwxyz123456789".toCharArray();
+        StringBuilder sb = new StringBuilder(10);
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        String output = sb.toString();
+        fileName = output;
+        out.close();
+        System.out.println(fileName);
+
         InputStream fileContent = filePart.getInputStream();
         response.sendRedirect(request.getContextPath() + "/table2.jsp");
         System.out.println(fileName);

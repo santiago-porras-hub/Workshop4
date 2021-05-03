7f96500bc0a8ab119a8da449897b104a2c812157
Omar Felipe Ladino Guezguan <oladinog@unbosque.edu.co>
2021-05-03 18:30:16 -0500

    Adding and fixing details

--- src/main/java/edu/unbosque/Workshop4/lista.java
+++ src/main/java/edu/unbosque/Workshop4/lista.java
@@ -4,28 +4,21 @@ public  class lista {
 
     String name = "";
     String comentario ="";
-
     public lista(String name, String comentario) {
         this.name = name;
         this.comentario = comentario;
     }
-
     public lista() {
-
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getComentario() {
         return comentario;
     }
-
     public void setComentario(String comentario) {
         this.comentario = comentario;
     }
